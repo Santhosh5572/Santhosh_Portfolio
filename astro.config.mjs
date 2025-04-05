249b1b10f6a31c25c739fb9405efcbb9ae61c807
Santhosh5572 <santhoshloganathan19@gmail.com>
2025-04-05 14:49:25 +0530

    Developer commit .Created Homepage .created Aboutpage .Created Myworks Created contact page

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+
+  devToolbar: {
+    enabled: false,
+  },
+
+  integrations: [react()],
+  output: 'server',
+  adapter: netlify(),
+});
