c753b90be4e9b4b24072f33efc6b0c0d71e416e7
Santhosh5572 <santhoshloganathan19@gmail.com>
2025-04-09 13:29:10 +0530

    Developer Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// import vercel from '@astrojs/vercel/static'
+
 import react from '@astrojs/react';
 
 import netlify from '@astrojs/netlify';
@@ -9,6 +11,7 @@ import netlify from '@astrojs/netlify';
 export default defineConfig({
   server: {
     port: 3000,
+    // adapter: vercel()
   },
 
   devToolbar: {
